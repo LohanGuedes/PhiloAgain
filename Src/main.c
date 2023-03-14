208ee605d3a5e8eccc2f95b09559574aa9e245ad
Lohan Guedes <lguedes@c2t1s3.42.rio>
2023-03-14 00:55:07 -0300

    Adding libft

--- Src/main.c
+++ Src/main.c
@@ -10,7 +10,6 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../Includes/libft/src/libft.h"
 #include "philo.h"
 
 void  *routine() {
